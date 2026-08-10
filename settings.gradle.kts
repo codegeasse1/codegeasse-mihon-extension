d5345e4632755604312706abc220f4551470f893
codegeasse <chattered199@gmail.com>
2026-08-10 15:10:22 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "mihon-extension-template"
 // Register every extension module here.
 // Convention: src/<lang>/<extension-name>
 include(":src:en:example")
+include(":src:en:comix")
