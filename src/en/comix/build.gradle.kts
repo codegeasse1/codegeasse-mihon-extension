1bba699824c5c61c2407ca1c7ab9eb9987cef686
codegeasse <chattered199@gmail.com>
2026-08-10 19:40:34 +0530

    Update build.gradle.kts

--- src/en/comix/build.gradle.kts
+++ src/en/comix/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 // --- Extension metadata -----------------------------------------------
 val extName = "Comix"
 val extClass = ".Comix"
-val extVersionCode = 1
+val extVersionCode = 2
 val isNsfw = false
 // ------------------------------------------------------------------------
 
