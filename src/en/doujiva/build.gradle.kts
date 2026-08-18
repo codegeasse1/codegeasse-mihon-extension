c4a2738b7a1bcf9e09665df23862164d29a35890
codegeasse <chattered199@gmail.com>
2026-08-18 21:14:56 +0530

    chore(doujiva): bump versionCode to 2

--- src/en/doujiva/build.gradle.kts
+++ src/en/doujiva/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 // --- Extension metadata -----------------------------------------------
 val extName = "Doujiva"
 val extClass = ".Doujiva"
-val extVersionCode = 1
+val extVersionCode = 2
 val isNsfw = true
 // ----------------------------------------------------------------------
 
