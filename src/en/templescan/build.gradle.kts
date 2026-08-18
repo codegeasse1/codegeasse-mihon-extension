d3cf0d9726bad82b37cb1fc05412c429a51c68a9
codegeasse <chattered199@gmail.com>
2026-08-19 02:58:03 +0530

    Bump TempleScan to v1.4.2 (publish fixed seriesData/chapter parsing to existing users)

--- src/en/templescan/build.gradle.kts
+++ src/en/templescan/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 // --- Extension metadata -----------------------------------------------
 val extName = "TempleScan"
 val extClass = ".TempleScan"
-val extVersionCode = 1
+val extVersionCode = 2
 val isNsfw = true
 // ----------------------------------------------------------------------
 
